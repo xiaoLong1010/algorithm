08100d1e679364282973df7d42c1b41b5608d261
chenlong11 <chenlong_2014@sina.com>
2019-01-09 17:42:16 +0800

    add SetMap Project

---
+++ LeetCode/SetMap/SetMap/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  SetMap
+//
+//  Created by Csy on 2019/1/9.
+//  Copyright © 2019 CXL. All rights reserved.
+//
+
+import Foundation
+
+func test15_ThreeSum() -> Void {
+    let answer = ThreeSum()
+    
+    let result = answer.solution1([-1, 0, 1, 2, -1, -4])
+    print(result)
+}
+test15_ThreeSum()
+
