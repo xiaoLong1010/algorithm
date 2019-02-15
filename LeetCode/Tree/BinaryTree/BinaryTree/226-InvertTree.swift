803f5470f54f14af9c301be629f0b498d34e7a6f
chenlong11 <chenlong_2014@sina.com>
2019-02-15 13:25:24 +0800

    add leetcode 226

---
+++ LeetCode/Tree/BinaryTree/BinaryTree/226-InvertTree.swift
@@ -0,0 +1,30 @@
+//
+//  226-InvertTree.swift
+//  BinaryTree
+//
+//  Created by Csy on 2019/2/15.
+//  Copyright © 2019 CXL. All rights reserved.
+//
+
+/**
+ 翻转一棵二叉树。
+ */
+
+import Foundation
+
+class InvertTree {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        if root == nil {
+            return nil
+        }
+        
+        // 先反转左子树和又子树
+        _ = self.invertTree(root?.left)
+        _ = self.invertTree(root?.right)
+        
+        // 再反转当前结点的左右子结点
+        (root!.left, root!.right) = (root!.right, root!.left)
+        
+        return root
+    }
+}
