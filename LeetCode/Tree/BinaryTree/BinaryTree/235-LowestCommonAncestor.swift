f46a39de7cc312fffacf8d2f48e9aff7ed87e1c1
chenlong11 <chenlong_2014@sina.com>
2019-02-16 17:07:34 +0800

    add leetcode 235

---
+++ LeetCode/Tree/BinaryTree/BinaryTree/235-LowestCommonAncestor.swift
@@ -0,0 +1,37 @@
+//
+//  235-LowestCommonAncestor.swift
+//  BinaryTree
+//
+//  Created by Csy on 2019/2/16.
+//  Copyright © 2019 CXL. All rights reserved.
+//
+
+/**
+ 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ */
+
+import Foundation
+
+class LowestCommonAncestor {
+    func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+        if !(root != nil && p != nil && q != nil) {
+            return nil
+        }
+        
+        let value1 = p!.val
+        let value2 = q!.val
+        let rootValue = root!.val
+        
+        // 两个结点小于root，那公共结点就在左边
+        if rootValue > value1 && rootValue > value2  {
+            return lowestCommonAncestor(root?.left, p, q)
+        }
+        
+        // 两个结点大于root，那公共结点就在右边
+        if rootValue < value1 && rootValue < value2  {
+            return lowestCommonAncestor(root?.right, p, q)
+        }
+        
+        return root
+    }
+}
