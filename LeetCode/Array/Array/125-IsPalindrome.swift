7e73a625a73dc6e3c7ef12bb4156c602dc967744
chenlong11 <chenlong_2014@sina.com>
2019-03-02 10:08:02 +0800

    update leetcode Array

--- LeetCode/Array/Array/125-IsPalindrome.swift
+++ LeetCode/Array/Array/125-IsPalindrome.swift
@@ -6,6 +6,15 @@
 //  Copyright © 2019 CXL. All rights reserved.
 //
 
+/**
+ 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ 
+ 说明：本题中，我们将空字符串定义为有效的回文串。
+ 
+ 输入: "A man, a plan, a canal: Panama"
+ 输出: true
+ */
+
 import Foundation
 
 class IsPalindrome {
--- LeetCode/Array/Array/169-MajorityElement.swift
+++ LeetCode/Array/Array/169-MajorityElement.swift
@@ -6,6 +6,15 @@
 //  Copyright © 2019 CXL. All rights reserved.
 //
 
+/**
+ 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ 你可以假设数组是非空的，并且给定的数组总是存在众数。
+ 
+ 输入: [3,2,3]
+ 输出: 3
+ 
+ */
+
 import Foundation
 
 class MajorityElement {
