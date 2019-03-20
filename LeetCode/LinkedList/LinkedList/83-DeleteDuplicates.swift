6844225d68e42b5df7b28c77c81b0bb3d3e461e5
chenlong11 <chenlong_2014@sina.com>
2019-03-20 19:42:18 +0800

    update leetcode 83

--- LeetCode/LinkedList/LinkedList/83-DeleteDuplicates.swift
+++ LeetCode/LinkedList/LinkedList/83-DeleteDuplicates.swift
@@ -31,13 +31,12 @@ class DeleteDuplicates {
                 node2 = node2?.next
             }
             
-            // node1
+            // node1指向下一个不同的数
             node1?.next = node2
-            
+            // 两个指针向前走一步
             node1 = node2
             node2 = node2?.next
         }
-        
         node1?.next = node2
         
         return head
