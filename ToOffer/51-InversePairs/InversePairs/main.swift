2b8eb066d4f98247a2530c65ebd54635981e240d
chenlong11 <chenlong_2014@sina.com>
2019-03-23 19:14:30 +0800

    update tooffer  51

--- ToOffer/51-InversePairs/InversePairs/main.swift
+++ ToOffer/51-InversePairs/InversePairs/main.swift
@@ -66,6 +66,53 @@ func merge(_ nums: inout Array<Int>, start: Int, middle: Int, end: Int) -> Int {
     return count
 }
 
+func merge2(_ nums: inout Array<Int>, low: Int, middle: Int, high: Int) -> Int {
+    if low == high  {
+        return 0
+    }
+    
+    var left = low
+    var right = middle + 1
+    let length = high - low + 1
+    var sortedList = Array<Int>()
+    
+    var count = 0
+    // 左和右，两个子数组，每次都取最小值
+    while left <= middle && right <= high {
+        let leftValue = nums[left]
+        let rightValue = nums[right];
+        
+        if leftValue > rightValue {
+            sortedList.append(rightValue)
+            count += middle - left + 1      // 左边剩余的每个数，都和rightValue，组成逆序对
+            right += 1
+        } else {
+            sortedList.append(leftValue)
+            left += 1
+        }
+    }
+    
+    // 没有比较完的左子数组，放到sortedList
+    while left <= middle {
+        sortedList.append(nums[left])
+        left += 1
+    }
+    
+    // 没有比较完的右子数组，放到sortedList
+    while right <= high {
+        sortedList.append(nums[right])
+        right += 1
+    }
+    
+    for index in 0..<length {
+        nums[index + low] = sortedList[index]
+    }
+    
+    return count
+}
+
+
+
 func countSort(_ nums: inout Array<Int>, start: Int, end: Int) -> Int {
     if start >= end {
         return 0
@@ -74,17 +121,17 @@ func countSort(_ nums: inout Array<Int>, start: Int, end: Int) -> Int {
     let middle = (start + end) >> 1
     let leftCount = countSort(&nums, start: start, end: middle)
     let rightCount = countSort(&nums, start: middle + 1, end: end)
-    let count = merge(&nums, start: start, middle: middle, end: end)
+//    let count = merge(&nums, start: start, middle: middle, end: end)
+    let count = merge2(&nums, low: start, middle: middle, high: end)
+    
     
     return leftCount + rightCount + count
 }
 
-var nums1 = [7,5,6,4]
+var nums1 = [23,1,3,12,2,8,4]
 var count = countSort(&nums1, start: 0, end: nums1.count - 1)
 print(count)
 
 var nums2 = [5,6,7,8,1,2,3,4]
 count = countSort(&nums2, start: 0, end: nums2.count - 1)
 print(count)
-
-
