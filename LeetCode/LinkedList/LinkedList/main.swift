d842314d97b801e0a0a5d5f34472c490f708c87a
chenlong11 <chenlong_2014@sina.com>
2019-03-24 21:59:45 +0800

    update leetcode LinkedList test

--- LeetCode/LinkedList/LinkedList/main.swift
+++ LeetCode/LinkedList/LinkedList/main.swift
@@ -8,5 +8,16 @@
 
 import Foundation
 
-print("Hello, World!")
+var node1 = ListNode(1)
+var node2 = ListNode(2)
+var node3 = ListNode(3)
+var node4 = ListNode(4)
+var node5 = ListNode(5)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node4.next = nil
 
+let answer = ReverseBetween()
+answer.solution1(node1, 1, 3)
