b69404fc67eb9d4cce7c44f4c94b32cad8581397
chenlong11 <chenlong_2014@sina.com>
2019-03-31 18:03:33 +0800

    update leetcode Greedy

--- LeetCode/Greedy/Greedy/main.swift
+++ LeetCode/Greedy/Greedy/main.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+
 func test435_NonOverlappingIntervals() -> Void {
     let object = NonOverlappingIntervals()
     
@@ -16,14 +17,14 @@ func test435_NonOverlappingIntervals() -> Void {
     let result = object.solution1(intervals)
     print("result: ",result)
 }
-test435_NonOverlappingIntervals()
+//test435_NonOverlappingIntervals()
 
 func test455_AssignCookies() -> Void {
     let object = AssignCookies()
     
     let g = [1,2]
     let c = [1,2,3]
-    let result = object.solution1(g, c)
+    let result = object.findContentChildren(g, c)
     print("result: ",result)
 }
 //test455_AssignCookies()
