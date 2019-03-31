fbe8e07611a9912cb978e2aa92333f9820aaa26b
chenlong11 <chenlong_2014@sina.com>
2019-03-31 18:02:55 +0800

    update leetcode 435

--- LeetCode/Greedy/Greedy/435-NonOverlappingIntervals.swift
+++ LeetCode/Greedy/Greedy/435-NonOverlappingIntervals.swift
@@ -6,43 +6,57 @@
 //  Copyright © 2019 CXL. All rights reserved.
 //
 
+/**
+ 给定一个区间的集合，找到需要移除区间的最小数量，使剩余区间互不重叠。
+ 
+ 注意:
+ 可以认为区间的终点总是大于它的起点。
+ 区间 [1,2] 和 [2,3] 的边界相互“接触”，但没有相互重叠。
+ 
+ 思路：相当于求最长递增子序列
+
+ */
+
 import Foundation
+
+public class Interval {
+    public var start: Int
+    public var end: Int
+    
+    public init(_ start: Int, _ end: Int) {
+        self.start = start
+        self.end = end
+    }
+}
 
 
 class NonOverlappingIntervals {
-    /**
-     在每次选择中，区间的结尾最为重要，选择的区间结尾越小，留给后面的区间的空间越大，那么后面能够选择的区间个数也就越大
-     */
-    func solution1(_ intervals: Array<Array<Int>>) -> Int {
-        let intervals = intervals.sorted { (interval1, interval2) -> Bool in
-            let start1 = interval1[0]
-            let end1 = interval1[1]
-            let start2 = interval2[0]
-            let end2 = interval2[1]
-            
-            // end从小到大排列
-            if end1 != end2 {
-                return end1 < end2
-            }
-            return start1 < start2
+    
+    func eraseOverlapIntervals(_ intervals: [Interval]) -> Int {
+        let count = intervals.count
+        if count < 2 {
+            return 0
         }
-        let length = intervals.count
         
+        // end从小到大排列
+        let sortedIntervals = intervals.sorted { (interval1, interval2) -> Bool in
+            if interval1.end != interval2.end {
+                return interval1.end < interval2.end
+            }
+            return interval1.start < interval2.start
+        }
         
         // 初始值
-        var res = 1
+        var subCount = 1
         var pre = 0
         
-        for index in 1...(length - 1) {
-            let start = intervals[index][0]
-            
-            // 当前的start如果不小于之前的end，则数量加1
-            if start >= intervals[pre][1] {
-                res = res + 1
-                pre = index
+        for i in 1...(count - 1) {
+            // 当前的start大于之前的end
+            if sortedIntervals[i].start >= sortedIntervals[pre].end {
+                subCount += 1
+                pre = i
             }
         }
-        
-        return length - res
+        return count - subCount
     }
 }
